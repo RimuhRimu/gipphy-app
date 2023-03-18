f17e8da64c65641c816ff343c3e96210c7b20a3f
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2023-03-17 21:33:48 -0400

    feat(env): change env style

--- api/interfaces.ts
+++ api/interfaces.ts
@@ -1,11 +1,5 @@
 import { Bson } from 'mongodb'
 
-export interface envFile {
-	readonly username: string
-	readonly password: string
-	readonly key: JsonWebKey
-}
-
 export interface UserSchema {
 	readonly _id: Bson.ObjectId
 	username: string
--- api/utils.ts
+++ api/utils.ts
@@ -1,5 +1,5 @@
 import { ALGORITHM, ENV } from './const.ts'
-import { envFile } from './interfaces.ts'
+import 'dotenv'
 
 export function jsonToKey(json: JsonWebKey) {
 	return crypto
@@ -15,33 +15,33 @@ export function jsonToKey(json: JsonWebKey) {
 
 export async function getEnv() {
 	//retrieve environment variables
-	const envFile = Deno.readTextFileSync(ENV)
-	const envJson: envFile = JSON.parse(envFile)
+	let key = JSON.parse(Deno.env.get('KEY') || '')
+	const username = Deno.env.get('USERNAME')
+	const password = Deno.env.get('PASSWORD')
 
 	//if no key, generate a new one, e.g
 	// - executed in other machine
-	// - deleted the key field in .env.json
-	// - actually the .env.json was deleted
-	if (!envJson.key.k) {
-		const key = await crypto.subtle.generateKey(
+	// - deleted the key field in .env
+	// - deleted .env file
+	if (!Object.values(key).length) {
+		key = await crypto.subtle.generateKey(
 			ALGORITHM,
 			true,
 			['sign', 'verify'],
 		)
-		const keyExport = await crypto.subtle.exportKey('jwk', key)
 
-		//save new key
-		Deno.writeTextFileSync(ENV, JSON.stringify({ ...envJson, key: keyExport }))
 		return {
-			...envJson,
+			username: username,
+			password: password,
 			key,
 		}
 	}
 
 	//else return the actual environment variables
 	return {
-		...envJson,
-		key: await crypto.subtle.importKey('jwk', envJson.key, ALGORITHM, true, [
+		username: username,
+		password: password,
+		key: await crypto.subtle.importKey('jwk', key, ALGORITHM, true, [
 			'sign',
 			'verify',
 		]),
